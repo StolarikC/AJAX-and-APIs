6284e053f4ee1538e797de7436a2bbce7c48e993
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 09:31:55 -0500

    Add files via upload

---
+++ XMLHttpRequest/app.js
@@ -0,0 +1,17 @@
+// NOT IMPORTANT TO REMEMBER THIS CODE.  IT IS MAINLY FOR HISTORICAL CONTEXT.
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log("IT LOADED!!!");
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+};
+
+req.onerror = function () {
+    console.log("ERROR!!!");
+    console.log(this);
+};
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send;
